3fc8f58dc934e39a3283ef830022232f54c8c762
tak0231 <tryoo0607@gmail.com>
2022-05-04 17:58:17 +0900

    2022-05-04 백준 알고리즘 커밋1

---
+++ src/clear/Test1000.java
@@ -0,0 +1,16 @@
+package clear;
+import java.util.Scanner;
+
+public class Test1000 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc= new Scanner(System.in);
+		int A=sc.nextInt();
+		int B=sc.nextInt();
+		
+		System.out.println(A+B);
+
+	}
+
+}
